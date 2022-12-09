2a9fda49db1e786eda5a9879280a3980733b9c87
JsKandavel <88124580+JsKandavel@users.noreply.github.com>
2022-12-09 23:05:01 +0000

    2 files modified, 1 added

--- socket-server/index.js
+++ socket-server/index.js
@@ -1,3 +1,22 @@
 // run `node index.js` in the terminal
+const express = require('express');
+//const socket  = require('socket.io');
+const app = express();
+const port = 3000;
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+//console.log(`Hello Node.js v${process.versions.node}!`);
+app.post('/send-notification', (req, res) => {
+  const notify = { data: req.body };
+  socket.emit('notification', notify);
+  res.send(notify);
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server connection on  http://127.0.0.1:${port}`); // Server Connnected
+});
+
+const socket = requie('socket.io')(server);
+
+socket.on('connection', (socket) => {
+  console.log('Socket: client connected');
+});
