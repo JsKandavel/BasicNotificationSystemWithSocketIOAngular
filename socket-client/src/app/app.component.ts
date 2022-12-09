30b639787a2897f6a109a3e790eb297e57b71dea
JsKandavel <88124580+JsKandavel@users.noreply.github.com>
2022-12-09 23:42:00 +0000

    3 files modified, 1 added

--- socket-client/src/app/app.component.ts
+++ socket-client/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as io from 'socket.io-client';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'socket-client';
+  private socket: any;
+  public data: any;
+
+  constructor() {
+    //this.socket = io('http:127.0.0.1:3000');
+  }
+
+  public ngOnInit(): void {
+    this.socket.on('notification', (data: any) => {
+      this.data = data;
+    });
+  }
 }
--- socket-server/index.js
+++ socket-server/index.js
@@ -1,22 +1,20 @@
-// run `node index.js` in the terminal
 const express = require('express');
-//const socket  = require('socket.io');
 const app = express();
 const port = 3000;
 
-//console.log(`Hello Node.js v${process.versions.node}!`);
+// Send Notification API
 app.post('/send-notification', (req, res) => {
   const notify = { data: req.body };
-  socket.emit('notification', notify);
+  socket.emit('notification', notify); // Updates Live Notification
   res.send(notify);
 });
 
 const server = app.listen(port, () => {
   console.log(`Server connection on  http://127.0.0.1:${port}`); // Server Connnected
 });
-
-const socket = requie('socket.io')(server);
-
+// Socket Layer over Http Server
+const socket = require('socket.io')(server);
+// On every Client Connection
 socket.on('connection', (socket) => {
   console.log('Socket: client connected');
 });
