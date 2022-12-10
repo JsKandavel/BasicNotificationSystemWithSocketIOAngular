0bd7e3fa84b9e17b5fdad43a459fac7d4bc1fb3d
JsKandavel <88124580+JsKandavel@users.noreply.github.com>
2022-12-10 00:15:26 +0000

    1 binaries uploaded

---
+++ socket-server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+const cors = require('cors');
+
+var corsOptions = {
+  origin: 'http://localhost:4200',
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+
+app.use(cors());
+// Send Notification API
+app.post('/send-notification', cors(corsOptions), (req, res) => {
+  const notify = { data: req.body };
+  socket.emit('notification', notify); // Updates Live Notification
+  res.send(notify);
+});
+
+const server = app.listen(port, () => {
+  console.log(`Server connection on  http://127.0.0.1:${port}`); // Server Connnected
+});
+// Socket Layer over Http Server
+const socket = require('socket.io')(server);
+// On every Client Connection
+socket.on('connection', (socket) => {
+  console.log('Socket: client connected');
+});
